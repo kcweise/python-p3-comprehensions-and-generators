eb1b014c1e20028d3d2d04ee6f1b3793864876ce
Keenan Weise <kcweise@gmail.com>
2024-04-02 08:07:58 -0500

    Done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    new_list = [n for n in num_list if n % 2 == 0]
+    return new_list
     pass
 
 def make_exclamation(sentence_list):
+    new_list = [string + '!' for string in sentence_list]
+    return new_list
     pass
